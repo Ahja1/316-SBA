d28964e6f80006908c34a49d5b9b0ceff65b36d9
Ahja1 <ahjadias1@gmail.com>
2024-08-01 23:11:08 -0400

    commit

--- script.js
+++ script.js
@@ -17,6 +17,44 @@ para.innerText = "Please meet the criterea";
 
 document.body.appendChild(para);
 
+
+//Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+
+let text = document.getElementById("signup").innerText;
+document.getElementById("header").innerHTML = text; 
+
+
+
+// Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+
+
+ //Modify at least one attribute of an element in response to user interaction.
+
+const element = document.getElementById("myBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("Enrolltxt").innerHTML = "Thank You";
+}
+
+
+//Register at least two different event listeners and create the associated event handler functions.
+
+// Event listener for click to change text color
+element.addEventListener('click', changeTextColor);
+
+function changeTextColor() {
+    document.getElementById("Enrolltxt").style.color = "red";
+}
+
+// Event listener for mouseover to change background color
+element.addEventListener('mouseover', changeBackgroundColor);
+
+function changeBackgroundColor() {
+    element.style.backgroundColor = "lightgreen";
+}
+
+
 ////Use at least two Browser Object Model (BOM) properties or methods.
 
  // Event listener for click to change text and alert message
