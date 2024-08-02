753910ea9ecc079e9acae40124dbb459be19d5e5
Ahja1 <ahjadias1@gmail.com>
2024-08-01 23:03:48 -0400

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//Cache at least one element using selectElementById.
+
+document.getElementById("Enrolltxt").innerHTML = "Please Sign Up Below:";
+const reloadBtn = document.getElementById("reloadBtn");
